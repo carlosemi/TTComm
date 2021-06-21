eb80537cf784b338ce52560c42913d1cf57e842f
Carlos <carlos-egf@hotmail.com>
2021-06-21 17:28:24 -0500

    Minor Changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,9 @@ $(function(){
   $("#main").load("./src/components/ventas.html"); 
 });
 
+
+//Based on the click of the main buttons, it will change the div with id main to change screens and 
+//load other html files
 function vntFunction(){
 $("#main").load("./src/components/ventas.html");
 }
@@ -19,7 +22,12 @@ function fctFunction(){
   $("#main").load("./src/components/facturas.html");
 }
 
+function rptFunction(){
+  $("#main").load("./src/components/reportes.html");
+}
+
 //This is for selecting the table row
 $('#prd').on('click', 'tbody tr', function(event) {
   $(this).addClass('highlight').siblings().removeClass('highlight');
 });
+

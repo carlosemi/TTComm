652433e2796a2144fbe78c46fb9264dcb050af72
Carlos <carlos-egf@hotmail.com>
2021-06-30 12:49:01 -0500

    Added the get products functionality, now products are shown in the table

--- renderer.js
+++ renderer.js
@@ -6,42 +6,38 @@
 // process.
 
 const electron = require('electron');
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
 let $ = jQuery = require('jquery');
-
+const axios = require('axios')
 
 //This functions change the main component based on the menu button clicks
-$(function(){
-  $("#main").load("./src/components/ventas.html"); 
+$(function () {
+  $("#main").load("./src/components/ventas.html");
 });
 
 
 //Based on the click of the main buttons, it will change the div with id main to change screens and 
 //load other html files
-function vntFunction(){
-$("#main").load("./src/components/ventas.html");
+function vntFunction() {
+  $("#main").load("./src/components/ventas.html");
 }
 
-function prdFunction(){
-$("#main").load("./src/components/productos.html");
+function prdFunction() {
+  $("#main").load("./src/components/productos.html");
 }
 
-function cliFunction(){
+function cliFunction() {
   $("#main").load("./src/components/clientes.html");
 }
 
-function fctFunction(){
+function fctFunction() {
   $("#main").load("./src/components/facturas.html");
 }
 
-function rptFunction(){
+function rptFunction() {
   $("#main").load("./src/components/reportes.html");
 }
 
-//This is for selecting the table row
-$('#prd').on('click', 'tbody tr', function(event) {
-  $(this).addClass('highlight').siblings().removeClass('highlight');
-});
 
 //Open new Window when Add Product is clicked
 function productWindow() {
@@ -53,36 +49,50 @@ function productWindow() {
 //Get the existing Products and put them in a table
 async function getPrds() {
 
-  // await axios({
-  //   method: 'get',
-  //   url: 'http://localhost:5000/api/pos/addProduct',
-  //   headers: {'content-type': 'application/json' , 
-  //               'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjBkMjUwNTY1ZmVjODg0NTJjYzZhMWNlIn0sImlhdCI6MTYyNTAxMTEwM30.5Vr4INSKQUcnyl2CBx7NLKbDcQltuFR5Hv3qFVK9Afs'},
-  //   data: {
-  //     sku: codigo,
-  //     description: descripcion,
-  //     price: precio,
-  //     quantity: existencia
-  //   }
-  // })
-  // .then(function (response){
-  //   console.log(response.data)
+  var table = document.getElementById('prd');
+  var row;
 
-  //   if(response.data==='Success'){
-  //     document.getElementById("Success").textContent += `Success!!`
+  await axios({
+    method: 'get',
+    url: 'http://localhost:5000/api/pos/getProducts',
+    headers: {
+      'content-type': 'application/json',
+      'x-auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjBkMjUwNTY1ZmVjODg0NTJjYzZhMWNlIn0sImlhdCI6MTYyNTAxMTEwM30.5Vr4INSKQUcnyl2CBx7NLKbDcQltuFR5Hv3qFVK9Afs'
+    }
+  })
+    .then(function (response) {
 
-  //     //Wait 3 seconds before closing the window
-  //     setTimeout(function () {
-  //       // console.log("waited 3 seconds")
-  //       ipcRenderer.invoke('closeWnd').then((result) => {
-          
-  //       })
-  //     }, 3000)
-     
-      
-  //   }
-  // })
-  
+      console.log(response.data)
+      console.log(response.data.length)
+      console.log(response.data[0])
+
+      var sku, description, price, quantity;
+
+      for (var x = 0; x < response.data.length; x++) {
+        sku = response.data[x].sku
+        description = response.data[x].description
+        price = response.data[x].price
+        quantity = response.data[x].quantity
+
+        row = table.insertRow(x + 1);
+        var cell1 = row.insertCell(0)
+        var cell2 = row.insertCell(1)
+        var cell3 = row.insertCell(2)
+        var cell4 = row.insertCell(3)
+
+        cell1.innerHTML = sku
+        cell2.innerHTML = description
+        cell3.innerHTML = price
+        cell4.innerHTML = quantity
+      }
+
+    })
+
+    //This is for selecting the table row
+    $('#prd').on('click', 'tbody tr', function (event) {
+      $(this).addClass('highlight').siblings().removeClass('highlight');
+    });
+
 }
 
 
